9dba456516256ceb1166a31c51a2958003f97f6f
OmidReisi <omid79.dg@gmail.com>
2022-10-10 23:14:23 +0330

    MODIFY: add some comments

--- Basics/src/basic_django_project/blog/views.py
+++ Basics/src/basic_django_project/blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+# now that we're using html templates we don't need to import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here.
 
